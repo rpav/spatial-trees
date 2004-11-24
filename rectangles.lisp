dc34437fc6197ec679f8d7249af079af8e2f726f
csr21 <none>
2004-11-24 06:36:58 -0600

    fix for rectangles: all bounds are inclusive.
    darcs-hash:20041124123658-ed5a3-b974bad92b4d186f6ffc8dbf2cbf7796f56fd325.gz

--- rectangles.lisp
+++ rectangles.lisp
@@ -2,14 +2,12 @@
 
 ;;; Because of the need to represent infinite dimensions, a bound is
 ;;; either cl:- (representing negative infinity), cl:+ (representing
-;;; positive infinity) or a number (representing itself).  By
-;;; convention, lower bounds are inclusive and higher bounds are
-;;; exclusive; we currently do not have a way to specify otherwise.
-;;; (An exclusive upper bound of +\infty doesn't really make that much
-;;; sense, but it's functionally indistinguishable from an inclusive
-;;; upper bound for all except the degenerate rectangle with lower and
-;;; upper bounds at +\infty, and I suspect that this case never
-;;; arises in any way that matters.  FIXME nevertheless.)
+;;; positive infinity) or a number (representing itself).  All bounds
+;;; are inclusive at present; it's possible that this should change at
+;;; some point, but solutions of the form "lower bound -> inclusive,
+;;; upper -> exclusive" are undesireable for a number of reasons:
+;;; chiefly introduction of an asymmetry into an otherwise symmetric
+;;; space, and an inability to represent point data.
 
 (defun bound= (x y)
   (case x
@@ -76,8 +74,8 @@
 
 (defun %intersection/1d (l1 h1 l2 h2)
   (cond
-    ((and (bound<= l1 l2) (bound< l2 h1)) (cons l2 (boundmin h1 h2)))
-    ((and (bound<= l2 l1) (bound< l1 h2)) (cons l1 (boundmin h1 h2)))))
+    ((and (bound<= l1 l2) (bound<= l2 h1)) (cons l2 (boundmin h1 h2)))
+    ((and (bound<= l2 l1) (bound<= l1 h2)) (cons l1 (boundmin h1 h2)))))
 
 (defgeneric intersectp (one two))
 (defmethod intersectp ((r1 rectangle) (r2 rectangle))
