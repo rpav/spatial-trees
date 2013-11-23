7d840bdcd3bf8a58908dbbea394c75eeffcdc53b
Masataro Asai <guicho2.71828@gmail.com>
2013-11-23 21:59:24 +0900

    added FiveAM, asdf:test-system etc

--- spatial-tree-test.lisp
+++ spatial-tree-test.lisp
@@ -1,6 +1,19 @@
 ;;; Somewhat rudimentary tests of external functionality
 
-(in-package "SPATIAL-TREES-IMPL")
+(in-package :cl-user)
+(defpackage spatial-trees-test
+  (:use :cl
+        :spatial-trees
+        :spatial-trees-protocol
+        :rectangles
+        :fiveam)
+  (:shadowing-import-from :spatial-trees :delete :search :bounding-rectangle)
+  (:shadowing-import-from :rectangles :intersection))
+
+(in-package :spatial-trees-test)
+(def-suite :spatial-trees)
+(in-suite :spatial-trees)
+
 
 (defvar *kinds* '(:r :greene :r* :x))
 
@@ -11,79 +24,79 @@
          (hy (+ (random 1.0) ly)))
     (make-rectangle :lows (list lx ly) :highs (list hx hy))))
 
-(dolist (kind *kinds*)
-  (format *trace-output* "~&Random search for kind ~S..." kind)
-  (finish-output *trace-output*)
-  (let* ((list (loop repeat 1000 collect (make-random-rectangle)))
-         (tree (make-spatial-tree kind :rectfun #'identity)))
-    (dolist (r list)
-      (insert r tree))
-    (let* ((r (make-random-rectangle))
-           (result (search r tree))
-           (expected (remove-if-not (lambda (x) (intersectp x r)) list)))
-      (unless (null (set-difference result
+(test :random-search
+  (dolist (kind *kinds*)
+    (finishes
+      (let* ((list (loop repeat 1000 collect (make-random-rectangle)))
+             (tree (make-spatial-tree kind :rectfun #'identity)))
+        (dolist (r list)
+          (insert r tree))
+        (let* ((r (make-random-rectangle))
+               (result (search r tree))
+               (expected (remove-if-not (lambda (x) (intersectp x r)) list)))
+          (is (null (set-difference result
                                     expected
                                     :key (lambda (x)
                                            (list (lows x) (highs x)))
                                     :test #'equal))
-        (error "aargh: ~S and ~S differ" result expected))))
-  (format *trace-output* " passed.~%"))
+              "~&In random search for kind ~S: ~S and ~S differ" kind result expected))))))
 
-(dolist (kind *kinds*)
-  (format *trace-output* "~&Trisected search for kind ~S..." kind)
-  (finish-output *trace-output*)
-  (let* ((n 1000)
-         (list (loop repeat n collect (make-random-rectangle)
-                     collect (make-random-rectangle -2.0 -2.0)
-                     collect (make-random-rectangle 2.0 2.0)))
-         (tree (make-spatial-tree kind :rectfun #'identity)))
-    (dolist (r list)
-      (insert r tree))
-    (let ((r (make-rectangle :lows '(0.0 0.0) :highs '(1.0 1.0))))
-      ;; FIXME: find a way to test the relative speed of the following
-      ;; (sbcl-specifically if necessary).
-      (search r tree)
-      (remove-if-not (lambda (x) (intersectp x r)) list)
-      (assert (= (length (search r tree)) n))))
-  (format *trace-output* " passed.~%"))
+(test :trisected-search
+  (dolist (kind *kinds*)
+    (let* ((n 1000)
+           (list (loop repeat n collect (make-random-rectangle)
+                    collect (make-random-rectangle -2.0 -2.0)
+                    collect (make-random-rectangle 2.0 2.0)))
+           (tree (make-spatial-tree kind :rectfun #'identity)))
+      (dolist (r list)
+        (insert r tree))
+      (let ((r (make-rectangle :lows '(0.0 0.0) :highs '(1.0 1.0))))
+        ;; FIXME: find a way to test the relative speed of the following
+        ;; (sbcl-specifically if necessary).
+        (search r tree)
+        (remove-if-not (lambda (x) (intersectp x r)) list)
+        (is (= (length (search r tree)) n)
+            "In trisected search for kind ~S, ~s=~s and ~s=~s differ"
+            kind '(length (search r tree)) (length (search r tree))
+            'n n)))))
 
-(dolist (kind *kinds*)
-  (format *trace-output* "~&Arbitrary object search for kind ~S..." kind)
-  (finish-output *trace-output*)
-  (let* ((n 100)
-         (list (loop repeat n
-                     for r = (make-random-rectangle)
-                     collect (cons (lows r) (highs r))))
-         (rectfun (lambda (x) (make-rectangle :lows (car x) :highs (cdr x))))
-         (tree (make-spatial-tree kind :rectfun rectfun)))
-    (dolist (r list)
-      (insert r tree))
-    (let* ((r (make-random-rectangle))
-           (result (search r tree))
-           (expected (remove-if-not
-                      (lambda (x) (intersectp (funcall rectfun x) r))
-                      list)))
-      (unless (null (set-difference result expected
-                                    :key (lambda (x)
-                                           (let ((r (funcall rectfun x)))
-                                             (list (lows r) (highs r))))
-                                    :test #'equal))
-        (error "aargh: ~S and ~S differ" result expected))))
-  (format *trace-output* " passed.~%"))
+(test :arbitrary-object-search
+  (dolist (kind *kinds*)
+    (let* ((n 100)
+           (list (loop repeat n
+                    for r = (make-random-rectangle)
+                    collect (cons (lows r) (highs r))))
+           (rectfun (lambda (x) (make-rectangle :lows (car x) :highs (cdr x))))
+           (tree (make-spatial-tree kind :rectfun rectfun)))
+      (dolist (r list)
+        (insert r tree))
+      (let* ((r (make-random-rectangle))
+             (result (search r tree))
+             (expected (remove-if-not
+                        (lambda (x) (intersectp (funcall rectfun x) r))
+                        list)))
+        (is (null (set-difference
+                   result expected
+                   :key (lambda (x)
+                          (let ((r (funcall rectfun x)))
+                            (list (lows r) (highs r))))
+                   :test #'equal))
+            "Arbitrary object search for kind ~S: ~S and ~S differ"
+            kind result expected)))))
 
-(dolist (kind *kinds*)
-  (format *trace-output* "~&Deletion test for kind ~S..." kind)
-  (finish-output *trace-output*)
-  (let* ((n 100)
-         (list (loop repeat n collect (make-random-rectangle)))
-         (tree (make-spatial-tree kind :rectfun #'identity)))
-    (dolist (r list)
-      (insert r tree))
-    (dolist (r (cdr list))
-      (delete r tree))
-    (let* ((results (search (car list) tree))
-           (length (length results)))
-      (unless (= (length results) 1)
-        (error "aargh: wrong amount of stuff (~D entries) in ~S"
-               length tree))))
-  (format *trace-output* " passed.~%"))
+
+(test :deletion
+  (dolist (kind *kinds*)
+    (let* ((n 100)
+           (list (loop repeat n collect (make-random-rectangle)))
+           (tree (make-spatial-tree kind :rectfun #'identity)))
+      (dolist (r list)
+        (insert r tree))
+      (dolist (r (cdr list))
+        (delete r tree))
+      (let* ((results (search (car list) tree))
+             (length (length results)))
+        (is (= (length results) 1)
+            "aargh: wrong amount of stuff (~D entries) in ~S"
+            length tree)))))
+
---
+++ spatial-trees-test.asd
@@ -0,0 +1,14 @@
+;;; -*- mode: lisp -*-
+(asdf:defsystem :spatial-trees-test
+  :depends-on (:spatial-trees :fiveam)
+  :components
+  ((:file "spatial-tree-test"))
+  :perform (load-op :after (op c) 
+		    (eval (read-from-string "(fiveam:run! :spatial-trees)"))
+		    (asdf:clear-system c)))
+
+(asdf:defsystem :spatial-trees-viz
+  :depends-on (:spatial-trees :fiveam)
+  :components
+  ((:file "spatial-tree-viz")))
+
